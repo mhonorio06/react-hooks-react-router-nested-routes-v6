65d26de6d7f93cd6b397527eb92f37bb93ec779c
Manny <mhfernan35277@gmail.com>
2024-03-06 03:47:33 -0700

    code along

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import NavBar from "./components/NavBar";
+import { useState, useEffect} from 'react';
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+    .then(res => res.json())
+    .then(data => setUsers(data))
+    .catch(error => console.error(error))
+  }, [])
   return (
     <>
       <header>
-        
+        <NavBar/>
       </header>
+      <Outlet context={users}/>
     </>
   );
 };
